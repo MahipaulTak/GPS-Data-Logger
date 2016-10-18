77c0c6daedd9dd1ad9ae6dfc4120126894c371b1
Helldogify <mahipaul.tak@gmail.com>
2016-10-18 15:21:47 -0600

    stuff

---
+++ Desktop SD function(s)/SDfunctions.cpp
@@ -0,0 +1,10 @@
+#include <vector.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void GetGpsData(string filename)
+{
+  //figure out how to do gps plotting and return data from file in an applicable format
+}
