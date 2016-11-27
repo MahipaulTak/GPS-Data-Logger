30f1f4a84bbd43c3cda57369044e23086ebe25aa
Kevin Widmann <k.widmann@hotmail.com>
2016-11-26 18:12:27 -0700

    Update create_unique_filename.cpp

--- create_unique_filename.cpp
+++ create_unique_filename.cpp
@@ -1,6 +1,9 @@
 #include "function_prototypes.h"
 #include <SD.h>
 
+//Takes the number of files, n, on the SD card and makes a cstring which is just "n".
+//Will end up with file names "0", "1", "2" on the SD card
+//But this function just creates the name, not the actual file.
 void create_unique_filename(char *buffer){
   unsigned int file_count = 0;
   File root = SD.open("/", FILE_READ);
